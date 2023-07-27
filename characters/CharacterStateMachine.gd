19bdcfa0844b7afbe71989dd06889a16f8da6c89
RStep1 <stepanrykov.itmo@gmail.com>
2023-07-27 17:33:01 +0700

    feat: add auto setting states in state machine

--- characters/CharacterStateMachine.gd
+++ characters/CharacterStateMachine.gd
@@ -1,4 +1,11 @@
 class_name CharacterStateMachine
 extends Node
 
-var states : Array[State]
+@export var states : Array[State]
+
+func _read():
+	for child in get_children():
+		if child is State:
+			states.append(child)
+		else:
+			push_warning("Chiled " + child.name + " is not a State for CharacterStateMachine")
