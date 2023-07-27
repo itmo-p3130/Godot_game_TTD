2d936d73ae326bacbbe28b50e43070db1986385b
RStep1 <stepanrykov.itmo@gmail.com>
2023-07-27 17:00:59 +0700

    feat: set BlendSpace1D for idle and run animations

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -4,9 +4,17 @@ extends CharacterBody2D
 @export var speed : float =  300.0
 @export var jump_velocity : float = -400.0
 
+@onready var sprite : Sprite2D = $Sprite2D
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_tree : AnimationTree = $AnimationTree
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction : Vector2 = Vector2.ZERO
+var animation_locked : bool = false
+
+func _ready():
+	animation_tree.active = true
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,10 +27,27 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
+	direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction.x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	updatte_animation()
+	update_facing_direction()
+	
+func updatte_animation():
+	animation_tree.set("parameters/Move/blend_position", direction.x)
+	
+	if not animation_locked:
+		if direction.x != 0:
+			animated_sprite.play("run")
+		else:
+			animated_sprite.play("idle")
+			
+func update_facing_direction():
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+	elif direction.x < 0:
+		animated_sprite.flip_h = true
