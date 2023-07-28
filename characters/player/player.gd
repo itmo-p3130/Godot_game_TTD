0855b9e448548b6a11d4410121422816e92cf6b9
RStep1 <stepanrykov.itmo@gmail.com>
2023-07-28 16:11:34 +0700

    chore: remove extra lines from player script

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 @export var speed : float =  300.0
 @export var jump_velocity : float = -400.0
 
@@ -11,7 +10,6 @@ extends CharacterBody2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction : Vector2 = Vector2.ZERO
-var animation_locked : bool = false
 
 func _ready():
 	animation_tree.active = true
@@ -20,11 +18,6 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		#velocity.y = jump_velocity
-		pass
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -35,16 +28,14 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
-	updatte_animation()
+	update_animation_parameters()
 	update_facing_direction()
 	
-func updatte_animation():
+func update_animation_parameters():
 	animation_tree.set("parameters/Move/blend_position", direction.x)
-	
-			
+
 func update_facing_direction():
 	if direction.x > 0:
 		sprite.flip_h = false
 	elif direction.x < 0:
 		sprite.flip_h = true
-		
