1d64122290c1b7099d7fa11e5957579b34267221
Alexander Nalobin <m_troc@mail.ru>
2023-08-03 04:08:46 +0300

    fix: change type to CharacterBody2D, now it isn't fluttering while moving

--- objects/door/door.gd
+++ objects/door/door.gd
@@ -1,4 +1,4 @@
-extends AnimatableBody2D
+extends CharacterBody2D
 
 @export_category("Trigger")
 @export var trigger : Node2D
@@ -16,6 +16,7 @@ enum _State {idle, moving}
 @onready var _state: _State = _State.idle
 @onready var _direction : Vector2
 @onready var _consistant: bool
+@onready var _velocity  : Vector2 = Vector2(0, 0)
 
 func set_trigger(new_trigger: Node2D) -> void:
 	trigger = new_trigger
@@ -28,13 +29,14 @@ func get_trigger() -> bool:
 func _ready():
 	position = start_pos.position
 
-func _process(delta):
+func _physics_process(delta):
 	if get_trigger():
 		_direction = end_pos.position-position
 		move_to(delta, end_pos)
 	else:
 		_direction = start_pos.position-position
 		move_to(delta, start_pos)
+	move_and_collide(_velocity)
 	#position += move_speed * delta
 
 func setAnim(state: _State) -> void:
@@ -49,7 +51,10 @@ func setAnim(state: _State) -> void:
 func move_to(delta:float, pos:Node2D) -> void:
 	if position != pos.position:
 		_state = _State.moving
-		position += _direction.normalized() * delta * move_speed * 10
-		return
-	else: _state = _State.idle
+		_velocity = _direction.normalized() * delta * move_speed * 100
+		if _direction.length() < 2:
+			position = pos.position
+			_velocity = Vector2(0, 0)
+	else:
+		_state = _State.idle
 	setAnim(_state)
