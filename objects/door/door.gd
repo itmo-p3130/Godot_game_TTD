9937651ffa5b57f6cd4d41b0df29f7282d4b2a0b
Ivan Tolmachev <ivatolm@gmail.com>
2023-08-05 02:55:22 +0300

    refactor: use python's code style convention

--- objects/door/door.gd
+++ objects/door/door.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+
 @export_category("Trigger")
 @export var trigger : Node2D
 @export var switch_name : String
@@ -8,38 +9,42 @@ extends CharacterBody2D
 @export var anim_speed: float
 @export var move_speed: float
 @export var start_pos: Node
-@export var end_pos  : Node
+@export var end_pos: Node
 
 enum _State {idle, moving}
 
 @onready var _sprite = $AnimatedSprite2D
 @onready var _state: _State = _State.idle
-@onready var _direction : Vector2
-@onready var _consistant: bool
-@onready var _velocity  : Vector2 = Vector2(0, 0)
+@onready var _direction: Vector2
+@onready var _velocity: Vector2 = Vector2(0, 0)
+
 
 func set_trigger(new_trigger: Node2D) -> void:
 	trigger = new_trigger
+
 
 func get_trigger() -> bool:
 	if typeof(trigger.get(switch_name)) == TYPE_BOOL:
 		return trigger.get(switch_name)
 	return false
 
+
 func _ready():
 	position = start_pos.position
 
-func _physics_process(delta):
+
+func _physics_process(delta: float):
 	if get_trigger():
-		_direction = end_pos.position-position
+		_direction = end_pos.position - position
 		move_to(delta, end_pos)
 	else:
-		_direction = start_pos.position-position
+		_direction = start_pos.position - position
 		move_to(delta, start_pos)
 	move_and_collide(_velocity)
 	#position += move_speed * delta
 
-func setAnim(state: _State) -> void:
+
+func set_anim(state: _State) -> void:
 	if state == _State.idle:
 		_sprite.animation = "idle"
 	elif state == _State.moving:
@@ -49,7 +54,9 @@ func setAnim(state: _State) -> void:
 		print(get_name() + " : unknown _State error")
 	_sprite.set_speed_scale(anim_speed)
 	_sprite.play(_sprite.animation)
-func move_to(delta:float, pos:Node2D) -> void:
+
+
+func move_to(delta: float, pos: Node2D) -> void:
 	if position != pos.position:
 		_state = _State.moving
 		_velocity = _direction.normalized() * delta * move_speed * 100
@@ -58,4 +65,4 @@ func move_to(delta:float, pos:Node2D) -> void:
 			_velocity = Vector2(0, 0)
 	else:
 		_state = _State.idle
-	setAnim(_state)
+	set_anim(_state)
